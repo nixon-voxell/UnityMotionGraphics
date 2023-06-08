de20791cee9280033207118034fcc859d46c0eb5
Nixon <voxell.technologies@gmail.com>
2023-06-09 01:04:42 +0800

    init MXCommandBuffer

---
+++ Voxell.MotionGFX.ECS/Core/MXCommandBuffer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Voxell.MotionGFX.ECS
+{
+    public class MXCommandBuffer : System.IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+
+    public struct MXCommand
+    {
+        
+    }
+}
